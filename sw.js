797f60f451455a942066b82123e859d7a6e73d4b
zoki02122 <zoran22051967@gmail.com>
2026-09-09 20:57:24 +0200

    Dodaj sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'apoteka-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((networkResp) => {
+        if(networkResp && networkResp.status === 200){
+          const clone = networkResp.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return networkResp;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
